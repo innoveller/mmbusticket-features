beb9a59cdafd8882817d6dd3a5e531d363c78aa6
Kyaw Soe Han <kokyawsoehan@gmail.com>
2025-03-21 13:47:42 +0630

    added custom feature extenstion to 11ty config

---
+++ eleventy.config.js
@@ -0,0 +1,25 @@
+export default function (eleventyConfig) {
+	// Add as a valid extension to process
+	// Alternatively, add this to the list of formats you pass to the `--formats` CLI argument
+	eleventyConfig.addTemplateFormats("feature");
+
+	// "feature" here means that the extension will apply to any .feature file
+	eleventyConfig.addExtension("feature", {
+        outputFileExtension: "html", // optional, default: "html"
+		compile: async (inputContent) => {
+			// Replace any instances of cloud with butt
+			let output = `
+            <pre>
+              <code class="language-gherkin"  id="response-div">
+                ${inputContent}
+              </code>
+            </pre>
+            `;
+
+			return async () => {
+				return output;
+			};
+		},
+	});
+};
+
